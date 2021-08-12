520afb7201c67a785ece34a8bf6740469e083d84
mushypeas <qkrwoans2@gmail.com>
2021-08-12 18:16:07 +0900

    updated setup.sh so that the weights will be in the darknet directory

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 cd darknet
 make
-cd ..
 FILE=darknet53.conv.74
 if [ -f "$FILE" ]; then
     echo "$FILE already exists."
 else
     wget https://pjreddie.com/media/files/darknet53.conv.74
 fi
+cd ..
