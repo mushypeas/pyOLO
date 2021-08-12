a0f08f3d119de1cab8b46d55892d60c36bfb5300
mushypeas <qkrwoans2@gmail.com>
2021-08-12 17:17:15 +0900

    Some changes  - extensions list exported to extensions.txt  - background_trimmer.py renamed to image_trimmer.py  - created requirement.txt, settings.txt  - object data are now passed as dictionaries from pyOLO.py  - added Trimming modes in image_trimmer.py  - reorganized overall structure of image_trimmer.py

--- pyOLO.py
+++ pyOLO.py
@@ -3,36 +3,46 @@ from glob import glob
 from os import system as terminal
 
 from src.generate_db import GenerateDB
-from src.background_trimmer import Trim, CheckRequirements
+from src.image_trimmer import Trim, CheckRequirements
 
-EXTENSIONS = ["png", "jpg", "jpeg", "bmp"]
 BACKGROUND_SIZE = 608
 OBJECT_SIZE = 96
 DATASET_SIZE = 5
 
 if __name__ == "__main__":
     background_paths = []
-    object_paths = []
+    objects = []
 
+    extensions = open("extensions.txt", "r").read().splitlines()
+    settings_list = open("settings.txt", "r").read().splitlines()
+
+    for settings in settings_list:
+        object = {}
+        object["name"] = settings.split(" ")[0]
+        object["path"] = f"objects/{object['name']}"
+        object["mode"] =  settings.split(" ")[1]
+        objects.append(object)
+        
     # get image paths
-    for extension in EXTENSIONS:
+    for extension in extensions:
         background_paths += glob("backgrounds/*." + extension)
-    object_paths += glob("objects/*/")
-if int(sys.argv[1]) <= 1:
-    print("[Step 1] Trimming images")
+        
+    if int(sys.argv[1]) <= 1:
+        print("[Step 1] Trimming images")
 
-    # remove all previous data
-    terminal("rm -rf objects/*/out/*.png")
+        # remove all previous data
+        terminal("rm -rf objects/*/out/*.png")
 
-    CheckRequirements()
-    for object_path in object_paths:
-        Trim(object_path, "white", 50, 10, EXTENSIONS)
-    print("Done.")
-if int(sys.argv[1]) <= 2:
-    print("[Step 2] Generating Dataset")
+        CheckRequirements()
+        for object in objects:
+            Trim(object)
+        print("[Step 1] Done.")
 
-    # remove all previous data
-    terminal("rm -rf images/*/*.png images/*/*.txt")
+    if int(sys.argv[1]) <= 2:
+        print("[Step 2] Generating Dataset")
 
-    GenerateDB(background_paths, object_paths, BACKGROUND_SIZE, OBJECT_SIZE, DATASET_SIZE)
-    print("\nDone.")
+        # remove all previous data
+        terminal("rm -rf images/*/*.png images/*/*.txt")
+
+        GenerateDB(background_paths, objects, BACKGROUND_SIZE, OBJECT_SIZE, DATASET_SIZE)
+        print("[Step 2] Done.")
--- src/generate_db.py
+++ src/generate_db.py
@@ -4,9 +4,6 @@ from copy import copy
 
 import random
 import threading
-import time
-
-from cv2 import data
 
 data_idx = 0
 data_count = 0
@@ -19,7 +16,6 @@ def Resize(image, image_h, is_background):
 
 def GenerateData(_background, object_data_list, object_size, dataset_size):
     global data_idx
-
 
     for i in range(0, dataset_size):
         data_image = copy(_background)
@@ -66,7 +62,7 @@ def PrintLoading():
     while data_idx < data_count:
         print("Generating Data... [{}/{}]".format(data_idx+1, data_count), end="\r")
 
-def GenerateDB(background_paths, object_paths, background_size, object_size, dataset_size):
+def GenerateDB(background_paths, objects, background_size, object_size, dataset_size):
     global data_count, data_idx
 
     print("Loading Images...")
@@ -84,17 +80,17 @@ def GenerateDB(background_paths, object_paths, background_size, object_size, dat
     object_data_list = []
     object_id = 0
 
-    for object_path in object_paths:
-        object_name = object_path.split('/')[-2]
+    for object in objects:
+        object_name = object["name"]
         if object_name not in object_classes.keys():
             object_classes[object_name] = object_id
             object_id += 1
 
-        image_paths = glob("{}out/*.png".format(object_path))
+        image_paths = glob("{}/out/*.png".format(object["path"]))
         object_data_list.append((object_classes[object_name], image_paths))
-
 
     threading.Thread(target=PrintLoading).start()
 
     for background in backgrounds:
         GenerateData(background, object_data_list, object_size, dataset_size)
+    print("")
