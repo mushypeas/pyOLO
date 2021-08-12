fc4ea50e28c59689848f5c09be65928e02e7d16f
mushypeas <qkrwoans2@gmail.com>
2021-08-12 18:14:37 +0900

    updated setup.sh & darknet/.gitignore

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 cd darknet
 make
 cd ..
-wget https://pjreddie.com/media/files/darknet53.conv.74
+FILE=darknet53.conv.74
+if [ -f "$FILE" ]; then
+    echo "$FILE already exists."
+else
+    wget https://pjreddie.com/media/files/darknet53.conv.74
+fi
